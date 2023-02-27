8c2f9e1bf1f28e24ab8bc831e9ece09ca49db8f3
Ruttaphong Worachartudomphong <maggotgluon@gmail.com>
2023-02-26 21:12:59 -0800

    fix visual

--- resources/views/livewire/servey.blade.php
+++ resources/views/livewire/servey.blade.php
@@ -1,7 +1,7 @@
 <div class="text-white">
     <x-notifications />
     <x-dialog />
-    <img src="{{asset('assets/images/logo.png')}}" class="w-64"/>
+    <img src="{{asset('/assets/images/logo.png')}}" class="w-64 m-auto"/>
     <h2 class="text-5xl font-bold text-center">
         {{$question->Question}}
     </h2>
@@ -17,22 +17,28 @@
         <x-button  wire:click="save(1)" spinner="save" loading-delay="longest"
         class="servey-btn">
             <img src="/assets/images/score-1.png" alt="">
+            <span>1</span>
+            
         </x-button>
         <x-button  wire:click="save(2)" spinner="save" loading-delay="longest"
         class="servey-btn">
             <img src="/assets/images/score-2.png" alt="">
+            <span>2</span>
         </x-button>
         <x-button  wire:click="save(3)" spinner="save" loading-delay="longest"
         class="servey-btn">
             <img src="/assets/images/score-3.png" alt="">
+            <span>3</span>
         </x-button>
         <x-button  wire:click="save(4)" spinner="save" loading-delay="longest"
         class="servey-btn">
             <img src="/assets/images/score-4.png" alt="">
+            <span>4</span>
         </x-button>
         <x-button  wire:click="save(5)" spinner="save" loading-delay="longest"
         class="servey-btn">
             <img src="/assets/images/score-5.png" alt="">
+            <span>5</span>
         </x-button>
     </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 
 <x-guest-layout>
-<div class="w-screen h-screen grid items-center container-bg">
+<div class="w-screen h-screen grid items-center" style="background: url('/assets/images/body_bg.jpg');">
     <livewire:servey :question="1"/>
 </div>
 </x-guest-layout>
