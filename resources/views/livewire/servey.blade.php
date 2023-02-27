3e8df6a992a6bf95065d3a9b1ec27c8695f6337f
Ruttaphong Worachartudomphong <maggotgluon@gmail.com>
2023-02-27 11:39:11 +0700

    add images

--- resources/views/livewire/servey.blade.php
+++ resources/views/livewire/servey.blade.php
@@ -1,12 +1,14 @@
-<div>
+<div class="text-white">
     <x-notifications />
     <x-dialog />
+    <img src="{{asset('assets/images/logo.png')}}" class="w-64"/>
     <h2 class="text-5xl font-bold text-center">
         {{$question->Question}}
     </h2>
 
     <div class="{{$udid?'opacity-0':'opacity-100'}} mx-32 my-10">
         <x-input wire:model="udid" id="udid" label="Scan Wrist Band"
+                class="text-center"
                 placeholder="Please, scan your wrist band" autofocus/>
     </div>
 
@@ -14,23 +16,23 @@
     <div class="w-full h-80 flex justify-around items-center my-16">
         <x-button  wire:click="save(1)" spinner="save" loading-delay="longest"
         class="servey-btn">
-            <img src="http://placekitten.com/300/300" alt="">
+            <img src="/assets/images/score-1.png" alt="">
         </x-button>
         <x-button  wire:click="save(2)" spinner="save" loading-delay="longest"
         class="servey-btn">
-            <img src="http://placekitten.com/300/300" alt="">
+            <img src="/assets/images/score-2.png" alt="">
         </x-button>
         <x-button  wire:click="save(3)" spinner="save" loading-delay="longest"
         class="servey-btn">
-            <img src="http://placekitten.com/300/300" alt="">
+            <img src="/assets/images/score-3.png" alt="">
         </x-button>
         <x-button  wire:click="save(4)" spinner="save" loading-delay="longest"
         class="servey-btn">
-            <img src="http://placekitten.com/300/300" alt="">
+            <img src="/assets/images/score-4.png" alt="">
         </x-button>
         <x-button  wire:click="save(5)" spinner="save" loading-delay="longest"
         class="servey-btn">
-            <img src="http://placekitten.com/300/300" alt="">
+            <img src="/assets/images/score-5.png" alt="">
         </x-button>
     </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 
 <x-guest-layout>
-<div class="w-screen h-screen grid items-center">
+<div class="w-screen h-screen grid items-center container-bg">
     <livewire:servey :question="1"/>
 </div>
 </x-guest-layout>
